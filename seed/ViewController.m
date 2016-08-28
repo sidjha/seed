c5cb3faeb02b724aa274639123b4c07cf0b67efb
Siddharth Jha <sid@sidjha.com>
2016-08-27 22:44:53 -0400

    Implemented majority of off-circle view.
    What's left:
    - to show message if there are no nearby circles.
    - to switch to in-circle view when you walk into circle.

--- seed/ViewController.m
+++ seed/ViewController.m
@@ -36,6 +36,30 @@
     
     [self askForPermission];
     
+    
+    // Observe changes to user's location on map
+    [self.mapView.userLocation addObserver:self forKeyPath:@"location"
+                               options:(NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld)
+                               context:NULL];
+    
+}
+
+// Pan and zoom map based on current location:
+// (from http://stackoverflow.com/questions/2473706/how-do-i-zoom-an-mkmapview-to-the-users-current-location-without-cllocationmanag)
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+    if ([self.mapView showsUserLocation]) {
+        [self panAndZoomMap];
+        
+    }
+}
+
+- (void) panAndZoomMap {
+    CLLocationCoordinate2D curLocation = CLLocationCoordinate2DMake(self.mapView.userLocation.coordinate.latitude, self.mapView.userLocation.coordinate.longitude);
+    
+    MKCoordinateRegion zoomRegion = MKCoordinateRegionMakeWithDistance(curLocation, 400, 400);
+    
+    [self.mapView setRegion:zoomRegion animated:YES];
 }
 
 - (void) askForPermission {
@@ -43,28 +67,27 @@
 }
 
 - (void) getNearbyCircles {
-    // TODO: Get nearby circles from web API
+    
     self.nearbyCircles = @[];
     
     NSString *URLString = [NSString stringWithFormat:@"http://0.0.0.0:5000/circles?lat=%@&lng=%@", @"37.774095", @"-122.416830"];
     
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializerWithReadingOptions:NSJSONReadingMutableContainers];
+    
     [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-        NSLog(@"JSON: %@", responseObject);
         
-        if ([responseObject valueForKey:@"in_circle"] == 0) {
-            // TODO: responseObject[@"circles"] => self.nearbyCircles
+        NSLog(@"Response: %@", responseObject);
+        
+        if ([[responseObject objectForKey:@"in_circle"]boolValue] == NO) {
+            NSLog(@"Not in circle, here's some nearby ones..");
             
-            NSInteger numCircles = [[responseObject valueForKey:@"circles"] count];
+            self.nearbyCircles = [responseObject objectForKey:@"circles"];
             
-            //NSLog(@"Not in circle, so parsing nearby %ld circles..", (long)numCircles);
+            [self setUpFences];
             
-            for (NSInteger i = 0; i < numCircles; i++) {
-                //NSArray *keys = @[@"id", @"point", @"radius", @"name", @"city"];
-                // TODO: do stuff, maybe
-            }
         } else {
-            // TODO: you're in a circle, so load individual circle view.
+            // TODO: change to in-circle view
         }
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         NSLog(@"Error: %@", error);
@@ -73,28 +96,29 @@
     /*
      // Don't know how much of this code from AFNetworking 2.x is required.
      
-    manager.securityPolicy.allowInvalidCertificates = YES;
-    
-    manager.requestSerializer = [AFJSONRequestSerializer serializer];
-    
-    [manager.requestSerializer setAuthorizationHeaderFieldWithUsername:authToken password:@"something"];
-    
-    manager.responseSerializer = [AFJSONResponseSerializer serializerWithReadingOptions:NSJSONReadingMutableContainers];
+     manager.securityPolicy.allowInvalidCertificates = YES;
+     
+     manager.requestSerializer = [AFJSONRequestSerializer serializer];
+     
+     [manager.requestSerializer setAuthorizationHeaderFieldWithUsername:authToken password:@"something"];
+     
+     manager.responseSerializer = [AFJSONResponseSerializer serializerWithReadingOptions:NSJSONReadingMutableContainers];
      */
     
 }
 
 - (void) setUpFences {
     NSLog(@"Setting up %lu fences..", (unsigned long)[self.nearbyCircles count]);
-
+    
     // for each nearby circle, set up fence, annotation and overlay
-    for(NSInteger i = 0; i <= [self.nearbyCircles count]; i++) {
-        CLLocationDegrees lat; // = [self.nearbyCircles objectAtIndex:i].center_lat;
-        CLLocationDegrees lng;
+    for(NSInteger i = 0; i < [self.nearbyCircles count]; i++) {
+        
+        CLLocationDegrees lat = [[self.nearbyCircles objectAtIndex:i][@"center_lat"] floatValue];
+        CLLocationDegrees lng = [[self.nearbyCircles objectAtIndex:i][@"center_lng"] floatValue];
         CLLocationCoordinate2D center = CLLocationCoordinate2DMake(lat, lng);
         
-        NSInteger radius; // = [self.nearbyCircles objectAtIndex:i].radius;
-        NSString *name; // = [self.nearbyCircles objectAtIndex:i].name
+        NSInteger radius = [[self.nearbyCircles objectAtIndex:i][@"radius"] intValue];
+        NSString *name = [self.nearbyCircles objectAtIndex:i][@"name"];
         
         CLRegion *region = [[CLCircularRegion alloc] initWithCenter:center radius:radius identifier:name];
         
@@ -112,23 +136,27 @@
 #pragma mark - CLLocationManagerDelegate methods
 
 - (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region {
-    
+    NSLog(@"Monitoring started for region: %@", region.description);
 }
 
 - (void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error {
-
+    NSLog(@"Monitoring failed for region: %@, error: %@", region.description, error.description);
 }
 
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
-
+    NSLog(@"Entered region: %@", region.description);
+    
+    // TODO: change to in-circle view
 }
 
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
-
+    NSLog(@"Exited region: %@", region.description);
+    
+    // TODO: change to off-circle view
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-
+    NSLog(@"Location manager failed. Error: %@", error.description);
 }
 
 - (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
