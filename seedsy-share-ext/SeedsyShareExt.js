b21a6cdc51c6e35290909fa20937deaf54d232db
Siddharth Jha <sid@sidjha.com>
2016-10-27 21:20:07 -0400

    Only getting necessary data from webpage in Share extension view.

--- seedsy-share-ext/SeedsyShareExt.js
+++ seedsy-share-ext/SeedsyShareExt.js
@@ -2,7 +2,7 @@ var aShareExt = function() {};
 
 aShareExt.prototype = {
     run: function(arguments) {
-        arguments.completionFunction({"pageURL": document.URL, "pageSource": document.documentElement.outerHTML, "title": document.title, "selection": window.getSelection().toString()});
+        arguments.completionFunction({"pageURL": document.URL, "title": document.title});
     }
 };
 
